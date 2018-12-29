2206496c4461755b8bbeefaf7a152c4d50459e8e
nastysizonenko <43203316+nastysizonenko@users.noreply.github.com>
2018-12-29 19:56:25 +0300

    Лабораторная работа №5

---
+++ Лабораторная №5.js
@@ -0,0 +1,63 @@
+/*упражнение 5.1*/
+var a = true
+var b = 7
+c = a + b
+console.log(c)
+c = a - b
+console.log (c)
+
+var a = true
+var b = false
+c = a + b
+console.log(c)
+c = a - b
+console.log (c)
+
+var a = 'hfeh'
+var b = false
+c = a + b
+console.log(c)
+c = a - b
+console.log (c)
+
+
+var a = 'ffff'
+var b = 'rrr'
+c = a + b
+console.log(c)
+c = a - b
+console.log (c)
+
+/*упражнение 5.2*/
+
+var A = [1,2,3]
+console.log (A[0]*A[1]*A[2])
+
+var A = [1,2,3]
+console.log (A[0]+A[1]+A[2])
+var A = [1,2,3]
+console.log (A[2] % A.length++)
+
+/*упражнение 5.3*/
+var p =6
+var a =1
+var b =2
+var c =3
+var s =Math.sqrt(p * (p - a) * (p - b) * (p - c))
+console.log("the result is:s= " + s)
+
+/*упражнение 5.4*/
+var x=4, y
+y=4*x++
+console.log(X)
+console.log(Y)
+
+var question = prompt ('В каком году ты поступил в КИПУ?', ' ')
+if (question != 2018) alert ( 'Не правильно!' )
+
+
+var question = prompt('В каком году поступил в КИПУ?', ' ')
+if (question != 2018)
+  alert( 'Не правильно!' )
+else
+  alert( 'Привет, первокурсник!' )
